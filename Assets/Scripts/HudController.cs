612ca01ddc917475097706fb2f2a4fa860b34c82
samathareddyValgubelli <samathareddy.valgubelli@gmail.com>
2020-04-26 21:40:37 +0530

    HUD and TextMeshpro commit
    - Imported Textmeshpro
    - Created HUD panel prefab and Hud Controller script

---
+++ Assets/Scripts/HudController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudController : MonoBehaviour
+{
+    public GameObject hudPanel;
+
+
+    private void Start()
+    {
+        hudPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {       
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Debug.Log("keypressed");
+
+            if (!hudPanel.activeInHierarchy)
+            {
+                hudPanel.SetActive(true);
+            }
+            else
+            {
+                hudPanel.SetActive(false);
+            }
+        }
+    }
+}
